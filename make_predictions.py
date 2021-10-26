3ada0770fb3201c78b11d906aa868e1c0968a622
Mike Walmsley <walmsleymk1@gmail.com>
2021-10-26 15:27:13 +0100

    Mike's changes

--- make_predictions.py
+++ make_predictions.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     file_format = 'png'
 
     # utility function to easily list the images in a folder.
-    unordered_image_paths = predict_on_images.paths_in_folder(Path(os.getcwd() + '/Scaled_images'), file_format=file_format, recursive=False)
+    unordered_image_paths = predict_on_images.paths_in_folder(Path('/scaled_images'), file_format=file_format, recursive=False)
 
     ## or maybe you already have a list from a catalog?
     # unordered_image_paths = df['paths']
