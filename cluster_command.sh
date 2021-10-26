f07d1ea10f4e5e91cb3b417c54a2a7207bd19e4f
Mike Walmsley <walmsleymk1@gmail.com>
2021-10-26 15:52:39 +0100

    More path tweaks

--- cluster_command.sh
+++ cluster_command.sh
@@ -18,9 +18,12 @@ PYTHON=/share/nas/walml/miniconda3/envs/zoobot/bin/python
 
 FITS_DIR=/share/nas/walml/galaxy_zoo/decals/dr5/fits_native/J000
 
-$PYTHON $ZOOBOT_DIR/creating_image_main.py \
-    --fits-dir $FITS_DIR \
-    --scale-factor 2 
+# $PYTHON $ZOOBOT_DIR/creating_image_main.py \
+#     --fits-dir $FITS_DIR \
+#     --scale-factor 2 
     
 $PYTHON $ZOOBOT_DIR/make_predictions.py \
-    --batch-size 128 
+    --batch-size 128 \
+    --scaled-dir /share/nas/walml/repos/understanding_galaxies/scaled_images \
+    --checkpoint-loc /share/nas/walml/repos/zoobot_test/data/pretrained_models/decals_dr_train_set_only_replicated/checkpoint \
+    --save-loc /share/nas/walml/repos/understanding_galaxies/results/scaled_image_predictions.csv
